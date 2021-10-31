a83536ea89b57f760b0ad202b1e548a7b1e5bb24
shuhaibzahir <yesshuhaib.ss@gmail.com>
2021-10-31 09:48:25 +0530

    this is updation for the menu bar

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,11 @@ const Header = () => {
     <div className="flex items-center justify-between flex-col sm:flex-row item-center m-5 h-auto">
       <div className="flex items-center justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="HOME" Icon={BadgeCheckIcon} />
-        <HeaderItem title="HOME" Icon={CollectionIcon} />
-        <HeaderItem title="HOME" Icon={LightningBoltIcon} />
-        <HeaderItem title="HOME" Icon={SearchIcon} />
-        <HeaderItem title="HOME" Icon={UserIcon} />
+        <HeaderItem title="SAVED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="LATEST" Icon={CollectionIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="USER" Icon={UserIcon} />
       </div>
       <Image src="http://links.papareact.com/ua6" width={200} height={100} />
     </div>
